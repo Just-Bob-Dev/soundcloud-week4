99ee51e7e9f6cb6195c0d8036c98f7adef720050
Robert Hutchins <rjhutchins22@gmail.com>
2017-06-19 20:36:35 -0400

    Update, and build of sound cloud app

--- soundcloud/main.js
+++ soundcloud/main.js
@@ -1,14 +1,68 @@
 /*
   Here is a guide for the steps you could take:
 */
-
+// 8538a1744a7fdaa59981232897501e04
+// 095fe1dcd09eb3d0e1d3d89c76f5618f
 // 1. First select and store the elements you'll be working with
+var submitButton = document.getElementById('submitButton');
+var inputTrack = document.getElementById('musicSearch');
+var click = false;
+
+submitButton.addEventListener('click', onClickFunction(inputTrack.value));
+
+// inputTrack.addEventListener('keypress', onEnter(e, inputTrack.value));
 
 
 // 2. Create your `onSubmit` event for getting the user's search term
 
+function onClickFunction(args){
+  click = true;
+  if (click = true){
+    searchAPI(args);
+  }
+}
+
+function onEnter(e, args){
+  let x = e.which || e.keycode;
+  if(e.keycode === 13 && args != ""){
+    searchAPI(args);
+  }
+  else {
+    return "Hey you need to put something in the input area."
+  }
+}
+
+
+
 
 // 3. Create your `fetch` request that is called after a submission
+
+console.log("your connected")
+
+function searchAPI(userString){
+  // Takes input and turns searchable string into a fetch call to api.
+  userString.replace(/\s+/g, '');
+
+  console.log(userString);
+
+  let apiCall = 'https://api.soundcloud.com/tracks/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=' + userString;
+
+  console.log(apiCall);
+   fetch(apiCall)
+   .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log("bad response: " + response.status);
+          return
+        }
+        response.json().then(function(data){
+           var calledData = data;
+          console.log(data);
+         })
+      }
+    )
+}
+
 
 
 // 4. Create a way to append the fetch results to your page
