6bce3a386d941e42ff01fd801142e70e50a5d9ee
Robert Hutchins <rjhutchins22@gmail.com>
2017-06-21 09:09:44 -0400

    continue designing of soundcloud app website

--- soundcloud/main.js
+++ soundcloud/main.js
@@ -3,22 +3,24 @@
 // 8538a1744a7fdaa59981232897501e04
 // 095fe1dcd09eb3d0e1d3d89c76f5618f
 // 1. First select and store the elements you'll be working with
-var submitButt = document.getElementById("subButton");
+var submitButton = document.getElementById("subButton");
 var inputTrack = document.getElementById("musicSearch");
+let markup;
 // let searchItem = inputTrack.value;
 // console.log(searchItem);
 // 2. Create your `onSubmit` event for getting the user's search term
 
-submitButt.addEventListener('click', function onSubmit(){
+submitButton.addEventListener('click', function onSubmit(){
   console.log("Button clicked");
   console.log(inputTrack.value)
   let searchItem = inputTrack.value;
   searchAPI(searchItem);
 });
 
-inputTrack.addEventListener('keypress', function onEnter(e){
+inputTrack.addEventListener('keypress', function(e){
   let x = e.which || e.keycode;
-  if(e.keycode === 13 && inputTrack.value != ""){
+  if(e.keycode === 13 && inputTrack.value != "" && inputTrack.value){
+
     console.log(inputTrack.value);
     searchAPI(inputTrack.value);
   }
@@ -27,19 +29,15 @@ inputTrack.addEventListener('keypress', function onEnter(e){
   }
 });
 
-
-
 // 3. Create your `fetch` request that is called after a submission
 
 console.log("you're connected");
 
 function searchAPI(userString){
   //Takes input and turns searchable string into a fetch call to api.
-  // userString.replace(/\s+/g, '');
-
   console.log(userString);
-
-   let apiCall = 'https://api.soundcloud.com/tracks/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=' + userString;
+  let clientId = '095fe1dcd09eb3d0e1d3d89c76f5618f&q'
+   let apiCall = 'https://api.soundcloud.com/tracks/?client_id='+ clientId + '=' + userString;
 
    fetch(apiCall)
    .then
@@ -51,29 +49,45 @@ function searchAPI(userString){
         }
         response.json().then(function(data){
           console.log(data);
-          appendSearchResults(data);
+          let myTracks = data;
 
+//4. Create a way to append the fetch results to your page
+
+          function renderMyTrack()
+          {
+            return`<div class="track-container">
+            ${myTracks.map(myTrack =>
+            `
+            <div class="track">
+              <img src="${myTrack.artwork_url}">
+              <p class="song-name">${myTrack.title}</p>
+              <h3 class="user-name">${myTrack.user.username}</h3>
+            </div>
+            `).join('')}
+            </div>`;
+          }
+          markup = renderMyTrack();
+          console.log(markup);
+          document.getElementById('results').innerHTML = markup;
         });
       }
+
     );
 }
 
 
 
-//4. Create a way to append the fetch results to your page
 
-function appendSearchResults(data){
-for(let i = 0;i<data.length; i++)
-{
-  module=
-  `
-  <div class="track">
-    <img class="track-artwork" src="${data.artwork_url}">
-    <h3 clas="song-name">${data.title}</h3>
-  </div>
-  `
-  document.getElementsByClassName("results").innerHTML += module;
-  }
-}
+//
+// function appendSearchResults(data){
+//
+//     module=`
+//       <div class="track">
+//       <img class="track-artwork" src="${data.artwork_url}">
+//       <h3 clas="song-name">${data.title}</h3>
+//       </div>
+//       `
+//       document.getElementsByClassName("results").innerHTML = module;
+// }
 
 // 5. Create a way to listen for a click that will play the song in the audio play
